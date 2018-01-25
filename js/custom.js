f2fb632b2921d2903c139cf2b1e814d9c4d229a1
Yujia-Wang <yujia.wang@hotmail.com>
2018-01-24 23:07:45 -0500

    big change

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,3 @@
-/* navbar */
-
-var lastScrollTop = 0;
-
-$(window).scroll(function(event) {
-	var st = $(this).scrollTop();
-
-	if(st > lastScrollTop) {
-		// Scroll down
-		$('nav').fadeOut().removeClass('nav-up');
-	} else {
-		// Scroll up
-		$('nav').fadeIn().addClass('nav-up');
-	}
-
-	lastScrollTop = st;
-});
-
-
 
 /* button */
 
@@ -43,3 +24,56 @@ function e_btnName() {
 		document.getElementById("evaluationBtn").innerHTML="See Evaluation Process";
 	}
 }
+
+$(document).ready(function() {
+
+	/* navbar */
+	var lastScrollTop = 0;
+
+	$(window).scroll(function(event) {
+		var st = $(this).scrollTop();
+
+		if(st > lastScrollTop) {
+			// Scroll down
+			$('nav').fadeOut().removeClass('nav-up');
+		} else {
+			// Scroll up
+			$('nav').fadeIn().addClass('nav-up');
+			if(st == 0) {
+				$('nav').removeClass('nav-up');
+			}
+		}
+
+		lastScrollTop = st;
+	});
+
+
+	/* back-to-top button */
+
+	$(window).scroll(function() {
+		if($(this).scrollTop() >= 200) {
+			$('.back2top-btn').fadeIn(200);
+		} else {
+			$('.back2top-btn').fadeOut(200);
+		}
+	});
+
+	$('.back2top-btn').click(function(event) {
+		event.preventDefault();
+
+		$('body,html').animate({scrollTop: 0}, 500);
+
+		return false;
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
