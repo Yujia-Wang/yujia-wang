9c80518418d9eaba5c17261ec2aec92f5d5def95
Yujia Wang <yujia.wang@hotmail.com>
2018-12-16 18:04:54 -0500

    homepage

--- js/custom.js
+++ js/custom.js
@@ -24,6 +24,8 @@ $(document).ready(function() {
 	// 		$("#evaluationBtn").text("See Evaluation Process");
 	// 	}
 	// });
+    
+    initComparisons();
 
 	/* navbar */
 	var lastScrollTop = 0;
@@ -47,13 +49,21 @@ $(document).ready(function() {
 	});
 
 	/* project */
+    
+    $("#foodex").click(function() {
+		location.href = "foodex.html"
+	});
+    
+    $("#vstyle").click(function() {
+		location.href = "vstyle.html"
+	});
 
 	$("#fittingeasy").click(function() {
 		location.href = "fittingeasy.html"
 	});
 
-	$("#wehome").click(function() {
-		location.href = "wehome.html"
+	$("#robustime").click(function() {
+		location.href = "robustime.html"
 	});
 
 	/* back-to-top button */
@@ -93,6 +103,54 @@ $(document).ready(function() {
 	// 	$(this).parent().siblings().children().removeClass("is-active");
 	// 	$("body, html").animate({scrollTop: $href.offset().top - 30}, 500);
 	// });
+    
+    $(window).resize(function() {
+        var imgWidth = $("#homepage-img1").css("width");
+        
+        $("#homepage-img2").css("width", imgWidth);
+    });
+    
+    function initComparisons() {
+        var x, i;
+        x = $(".img-comp-overlay");
+        
+        for (i = 0; i < x.length; i++) {
+            compareImages(x[i]);
+        }
+    }
+    
+    function compareImages(img) {
+        var imgWidth = $("#homepage-img1").css("width");
+        
+        $("#homepage-img2").css("width", imgWidth);
+        
+        var w;
+        var currentMousePos = { x: -1, y: -1 };
+        
+        w = img.offsetWidth;
+        console.log(w);
+        
+        img.style.width = (w / 2) + "px";
+        
+        $(document).mousemove(function(event) {
+            currentMousePos.x = event.pageX;
+            currentMousePos.y = event.pageY;
+            
+            var a, x = 0;
+            
+            a = img.getBoundingClientRect();
+            
+            x = currentMousePos.x - a.left;
+            
+            if (x < 0) { x = 0; }
+            if (x > w) { x = w; }
+            
+            img.style.width = x + "px";
+        });
+        
+        
+    }
+
 
 
 });
