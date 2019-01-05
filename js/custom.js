0c84081da7ddc37972915512aca0bb98c6e8e72c
Yujia Wang <yujia.wang@hotmail.com>
2019-01-04 21:06:19 -0500

    smooth scroll

--- js/custom.js
+++ js/custom.js
@@ -1,63 +1,12 @@
 $(document).ready(function() {
-
-	/* button */
-	// $("#researchBtn").click(function() {
-	// 	if($("#researchBtn").text() == "See Research Process") {
-	// 		$("#researchBtn").text("Hide Research Process");
-	// 	} else {
-	// 		$("#researchBtn").text("See Research Process");
-	// 	}
-	// });
-
-	// $("#designBtn").click(function() {
-	// 	if($("#designBtn").text() == "See Design Process") {
-	// 		$("#designBtn").text("Hide Design Process");
-	// 	} else {
-	// 		$("#designBtn").text("See Design Process");
-	// 	}
-	// });
-
-	// $("#evaluationBtn").click(function() {
-	// 	if($("#evaluationBtn").text() == "See Evaluation Process") {
-	// 		$("#evaluationBtn").text("Hide Evaluation Process");
-	// 	} else {
-	// 		$("#evaluationBtn").text("See Evaluation Process");
-	// 	}
-	// });
     
-//    ScrollReveal().reveal(".project", { delay: 500 });
-//    ScrollReveal().reveal(".intro", { delay: 500 });
-//    
-
-//    $('#fullpage').fullpage();
-    
-    
+    $("#fullpage").fullpage();
     
     initComparisons();
-
-	/* navbar */
-//	var lastScrollTop = 0;
-//
-//	$(window).scroll(function(event) {
-//		var st = $(this).scrollTop();
-//
-//		if(st > lastScrollTop) {
-//			// Scroll down
-//			$("nav").fadeOut();
-//		} else {
-//			// Scroll up
-//			$("nav").fadeIn().addClass("nav-up");
-//			if(st == 0) {
-//				$("nav").removeClass("nav-up");
-//			}
-//		}
-//
-//		lastScrollTop = st;
-//
-//	});
     
-
-	/* project */
+    /********************/
+	/*** project link ***/
+    /********************/
     
     $("#foodex").click(function() {
 		location.href = "foodex.html"
@@ -74,8 +23,10 @@ $(document).ready(function() {
 	$("#robustime").click(function() {
 		location.href = "robustime.html"
 	});
-
-	/* back-to-top button */
+    
+    /**************************/
+	/*** back-to-top button ***/
+    /**************************/
 
 	$(window).scroll(function() {
 		if($(this).scrollTop() >= 200) {
@@ -84,34 +35,10 @@ $(document).ready(function() {
 			$(".back2top-btn").fadeOut(200);
 		}
 	});
-
-	$(".back2top-btn").click(function(event) {
-		event.preventDefault();
-
-		$("body, html").animate({scrollTop: 0}, 500);
-
-		return false;
-	});
-
-	/* side navigation */
-
-	// $(window).scroll(function() {
-	// 	if($(this).scrollTop() >= 200) {
-	// 		$(".dotstyle").fadeIn(200);
-	// 	} else {
-	// 		$(".dotstyle").fadeOut(200);
-	// 	}
-	// });
-
-	// $(".dotstyle a").click(function(event) {
-	// 	event.preventDefault();
-
-	// 	var $href = $($(this).attr("href"));
-
-	// 	$(this).addClass("is-active");
-	// 	$(this).parent().siblings().children().removeClass("is-active");
-	// 	$("body, html").animate({scrollTop: $href.offset().top - 30}, 500);
-	// });
+    
+    /*********************************/
+    /*** project horizontal scroll ***/
+    /*********************************/
     
     $(window).resize(function() {
         var imgWidth = $("#homepage-img1").css("width");
@@ -154,12 +81,10 @@ $(document).ready(function() {
             if (x > w) { x = w; }
             
             img.style.width = x + "px";
-        });
-        
-        
+        }); 
     }
     
-    $("#fullpage").fullpage();
+    
 
 
 
